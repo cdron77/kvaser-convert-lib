3a35abed66e66be4d4e8503fe382bbf48e85f606
Davide Rondini <davide.rondini@samplesw.com>
2025-11-03 14:23:55 +0100

    Updated documentation of kvlibsdk.

--- kvlibsdk/doc/HTMLhelp/navtreedata.js
+++ kvlibsdk/doc/HTMLhelp/navtreedata.js
@@ -1,59 +1,60 @@
 var NAVTREE =
 [
-  [ "Welcome to Kvaser Linux Drivers and SDK!", "index.html", [
-    [ "Installation", "page_installing.html", "page_installing" ],
-    [ "Kvaser Support", "page_support.html", null ],
-    [ "License and Copyright", "page_license_and_copyright.html", null ],
-    [ "Compiling and Compatibility", "page_user_guide_build.html", "page_user_guide_build" ],
-    [ "Tutorials", "page_tutorial.html", "page_tutorial" ],
-    [ "CAN bus API (CANlib)", "page_canlib.html", "page_canlib" ],
-    [ "LIN bus API (LINlib)", "page_linlib.html", [
-      [ "Description", "page_linlib.html#section_user_guide_lin_lib_1", null ],
-      [ "Using the LIN Bus", "page_linlib.html#section_user_guide_lin_intro", null ],
-      [ "LIN Frame Identifiers", "page_linlib.html#section_user_guide_lin_frame_identifiers", null ],
-      [ "Where to go from here", "page_linlib.html#section_user_guide_lin_where_to_go_from_here", null ]
+  [ "Welcome to Kvaser Linux Drivers and SDK!", "index.htm", [
+    [ "Installation", "page_installing.htm", "page_installing" ],
+    [ "Kvaser Support", "page_support.htm", null ],
+    [ "License and Copyright", "page_license_and_copyright.htm", null ],
+    [ "Compiling and Compatibility", "page_user_guide_build.htm", "page_user_guide_build" ],
+    [ "Tutorials", "page_tutorial.htm", "page_tutorial" ],
+    [ "CAN bus API (CANlib)", "page_canlib.htm", "page_canlib" ],
+    [ "LIN bus API (LINlib)", "page_linlib.htm", [
+      [ "Description", "page_linlib.htm#section_user_guide_lin_lib_1", null ],
+      [ "Using the LIN Bus", "page_linlib.htm#section_user_guide_lin_intro", null ],
+      [ "LIN Frame Identifiers", "page_linlib.htm#section_user_guide_lin_frame_identifiers", null ],
+      [ "Where to go from here", "page_linlib.htm#section_user_guide_lin_where_to_go_from_here", null ]
     ] ],
-    [ "Database API (kvaDbLib)", "page_kvadblib.html", "page_kvadblib" ],
-    [ "Converter API (kvlclib)", "page_kvlclib.html", "page_kvlclib" ],
-    [ "Memorator API (kvmlib)", "page_kvmlib.html", [
-      [ "Description", "page_kvmlib.html#section_user_guide_kvmlib_1", null ],
-      [ "Naming convention", "page_kvmlib.html#section_user_guide_kvmlib_2", null ],
-      [ "Build an application", "page_kvmlib.html#section_user_guide_kvmlib_3", null ],
-      [ "Where to go from here", "page_kvmlib.html#section_user_guide_kvmlib_where_to_go_from_here", null ]
+    [ "Database API (kvaDbLib)", "page_kvadblib.htm", "page_kvadblib" ],
+    [ "Converter API (kvlclib)", "page_kvlclib.htm", "page_kvlclib" ],
+    [ "Memorator API (kvmlib)", "page_kvmlib.htm", [
+      [ "Description", "page_kvmlib.htm#section_user_guide_kvmlib_1", null ],
+      [ "Naming convention", "page_kvmlib.htm#section_user_guide_kvmlib_2", null ],
+      [ "Build an application", "page_kvmlib.htm#section_user_guide_kvmlib_3", null ],
+      [ "Where to go from here", "page_kvmlib.htm#section_user_guide_kvmlib_where_to_go_from_here", null ]
     ] ],
-    [ "Memorator XML API (kvaMemoLibXML)", "page_kvamemolibxml.html", [
-      [ "Description", "page_kvamemolibxml.html#section_user_guide_kvamemolibxml_1", null ],
-      [ "Build an application", "page_kvamemolibxml.html#section_user_guide_kvamemolibxml_2", null ],
-      [ "Where to go from here", "page_kvamemolibxml.html#section_user_guide_kvamemolibxml_where_to_go_from_here", null ]
+    [ "Memorator XML API (kvaMemoLibXML)", "page_kvamemolibxml.htm", [
+      [ "Description", "page_kvamemolibxml.htm#section_user_guide_kvamemolibxml_1", null ],
+      [ "Build an application", "page_kvamemolibxml.htm#section_user_guide_kvamemolibxml_2", null ],
+      [ "Where to go from here", "page_kvamemolibxml.htm#section_user_guide_kvamemolibxml_where_to_go_from_here", null ]
     ] ],
-    [ "Remote Device API (kvrlib)", "page_kvrlib.html", "page_kvrlib" ],
-    [ "Deprecated List", "deprecated.html", null ],
-    [ "Modules", "modules.html", "modules" ],
-    [ "Data Structures", "annotated.html", [
-      [ "Data Structures", "annotated.html", "annotated_dup" ],
-      [ "Data Fields", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Variables", "functions_vars.html", null ]
+    [ "Remote Device API (kvrlib)", "page_kvrlib.htm", "page_kvrlib" ],
+    [ "Deprecated List", "deprecated.htm", null ],
+    [ "Modules", "modules.htm", "modules" ],
+    [ "Data Structures", "annotated.htm", [
+      [ "Data Structures", "annotated.htm", "annotated_dup" ],
+      [ "Data Fields", "functions.htm", [
+        [ "All", "functions.htm", null ],
+        [ "Variables", "functions_vars.htm", null ]
       ] ]
     ] ],
     [ "Files", null, [
-      [ "File List", "files.html", "files" ]
+      [ "File List", "files.htm", "files" ]
     ] ],
-    [ "Examples", "examples.html", "examples" ]
+    [ "Examples", "examples.htm", "examples" ]
   ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"annotated.html",
-"canlib_8h.html#ab7c70409147e76675e586a7d201f8ae4",
-"examples.html",
-"group__kvadb__attributes.html#ga1698bce319bf3594798a940e1339235e",
-"group__kvadb__signals.html#ga734770b9b425260f1f9bb4dc5a886aaf",
-"group__kvm__disk__operations.html#gae3520ee83055783a046db21c8feae908",
-"kva_memo_lib_x_m_l_8h.html#a43075637b5455ecdf9412b4a1da4b6baabd55eb260a8dc51aa2f1857f350ffd4b",
-"linlib_8h.html#a2edd31e15abb5f7cb8dd213fd707dfff",
-"structcan_bus_statistics__s.html#a45656d1d249afb1ef001b9d56337b26b"
+"annotated.htm",
+"canlib_8h.htm#abc63331b227da4573dff86d7b119969c",
+"group___c_a_n.htm#gac01f98e282609b5f6aaf2b1eabfb83ec",
+"group__can__status__codes.htm#gga52b5e5c71832b0bd3c6a5b1fd48583e7afc8651b75676e525058c4289d823a12f",
+"group__kvadb__messages.htm#gaf396a8ddcdb19ceca7be58376ae10cd8",
+"group__kvlc__converter.htm#ga9dc12be7d0e933fb782bab86bca55666",
+"index.htm",
+"kvlclib_format__r_p_c_i_i_i.htm#kvlclib_section_rpciii_supported_properties",
+"page_user_guide_intro.htm#section_core_api_calls",
+"structkvm_log_trigger_ex.htm"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
